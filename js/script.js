81195f5f6ce3d8ed66264715c3cade9cf23cf72e
Jary Cruz <jarycruz@Sergios-MacBook-Pro.local>
2023-08-02 08:27:35 -0400

    Fixed Mobile Menu

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ const btnMenu = document.querySelector('[data-btn-menu]')
 const menu = document.querySelector('[data-menu]')
 
 btnMenu.addEventListener('click', () => {
-  btnMenu.classList.toggle('open')
+  const iconMenu = btnMenu.querySelector('i')
+
+  iconMenu.classList.toggle('fa-bars')
+  iconMenu.classList.toggle('fa-xmark')
   menu.classList.toggle('shown')
 })
