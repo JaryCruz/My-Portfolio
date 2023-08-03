749de3bb1907607d346c69f5a9696ce1caa9a1ba
Jary Cruz <jarycruz@Sergios-MacBook-Pro.local>
2023-08-03 12:48:43 -0400

    Added Light & Dark Theme

--- js/script.js
+++ js/script.js
@@ -1,10 +1,42 @@
 const btnMenu = document.querySelector('[data-btn-menu]')
+const iconMenu = btnMenu.querySelector('i')
+const btnTheme = document.querySelector('[data-btn-theme]')
+const iconTheme = btnTheme.querySelector('i')
 const menu = document.querySelector('[data-menu]')
+const DARK_THEME = 'dark'
+const LIGHT_THEME = 'light'
+const THEME_KEY = 'JARY_THEME'
 
-btnMenu.addEventListener('click', () => {
-  const iconMenu = btnMenu.querySelector('i')
+setTheme()
 
+function setTheme() {
+  if (localStorage.length === 0 || localStorage.getItem(THEME_KEY) === DARK_THEME) {
+    document.body.className = DARK_THEME
+    
+    iconTheme.classList.toggle('fa-sun')
+    iconTheme.classList.toggle('fa-moon')
+  } else {
+    document.body.className = LIGHT_THEME
+  }
+}
+
+function toggleMobileMenu() {
   iconMenu.classList.toggle('fa-bars')
   iconMenu.classList.toggle('fa-xmark')
+
   menu.classList.toggle('shown')
-})
+}
+
+function toggleTheme() {
+  iconTheme.classList.toggle('fa-sun')
+  iconTheme.classList.toggle('fa-moon')
+
+  document.body.classList.toggle(LIGHT_THEME)
+  document.body.classList.toggle(DARK_THEME)
+
+  localStorage.setItem('JARY_THEME', `${document.body.className === LIGHT_THEME ? LIGHT_THEME : DARK_THEME}`)
+}
+
+btnMenu.addEventListener('click', toggleMobileMenu)
+
+btnTheme.addEventListener('click', toggleTheme)
