1084541465e71b934fe9fe1dd43112c8cad813e4
Jary Cruz <jarycruz@proton.me>
2023-09-25 19:00:24 -0400

    Modified Sections

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const iconMenu = btnMenu.querySelector('i')
 const btnTheme = document.querySelector('[data-btn-theme]')
 const iconTheme = btnTheme.querySelector('i')
 const menu = document.querySelector('[data-menu]')
+const logo = document.querySelector('[data-logo]')
+const footerLogo = document.querySelector('[data-footer-logo]')
 const DARK_THEME = 'dark'
 const LIGHT_THEME = 'light'
 const THEME_KEY = 'JARY_THEME'
@@ -10,13 +12,18 @@ const THEME_KEY = 'JARY_THEME'
 setTheme()
 
 function setTheme() {
-  if (localStorage.length === 0 || localStorage.getItem(THEME_KEY) === DARK_THEME) {
+  if (
+    localStorage.length === 0 ||
+    localStorage.getItem(THEME_KEY) === DARK_THEME
+  ) {
     document.body.className = DARK_THEME
-    
+
     iconTheme.classList.toggle('fa-sun')
     iconTheme.classList.toggle('fa-moon')
   } else {
     document.body.className = LIGHT_THEME
+    logo.classList.add('white')
+    footerLogo.classList.add('white')
   }
 }
 
@@ -34,7 +41,13 @@ function toggleTheme() {
   document.body.classList.toggle(LIGHT_THEME)
   document.body.classList.toggle(DARK_THEME)
 
-  localStorage.setItem('JARY_THEME', `${document.body.className === LIGHT_THEME ? LIGHT_THEME : DARK_THEME}`)
+  logo.classList.toggle('white')
+  footerLogo.classList.toggle('white')
+
+  localStorage.setItem(
+    'JARY_THEME',
+    `${document.body.className === LIGHT_THEME ? LIGHT_THEME : DARK_THEME}`
+  )
 }
 
 btnMenu.addEventListener('click', toggleMobileMenu)
