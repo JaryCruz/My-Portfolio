8d5954c81089ec1aab3753694005842f7f0a1737
Jary Cruz <jarycruz@proton.me>
2023-11-26 09:12:10 -0500

    Added strategy section

--- js/script.js
+++ js/script.js
@@ -1,12 +1,7 @@
 const btnMenu = document.querySelector('[data-btn-menu]')
-const iconMenu = btnMenu.querySelector('i')
 const menu = document.querySelector('[data-menu]')
 
-function toggleMobileMenu() {
-  iconMenu.classList.toggle('fa-bars')
-  iconMenu.classList.toggle('fa-xmark')
-
+btnMenu.addEventListener('click', () => {
   menu.classList.toggle('shown')
-}
-
-btnMenu.addEventListener('click', toggleMobileMenu)
+  btnMenu.classList.toggle('open')
+})
