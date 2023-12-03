5a84ee9935386411619e3cda87f6414651608c9d
Jary Cruz <jarycruz@proton.me>
2023-12-03 08:28:59 -0500

    Removed test validation

--- js/script.js
+++ js/script.js
@@ -1,18 +1,5 @@
 const btnMenu = document.querySelector('[data-btn-menu]')
 const menu = document.querySelector('[data-menu]')
-const contactForm = document.querySelector('[data-contact-form]')
-const nameInput = document.querySelector('[data-name-input]')
-const inputError = document.querySelector('[data-error-input]')
-
-contactForm.addEventListener('submit', e => {
-  if (!nameInput.value) {
-    inputError.classList.add('shown')
-    e.preventDefault()
-  } else {
-    inputError.classList.remove('shown')
-    console.log('Shwon')
-  }
-})
 
 btnMenu.addEventListener('click', () => {
   menu.classList.toggle('shown')
